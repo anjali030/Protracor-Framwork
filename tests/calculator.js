a46a52685f9cc2813cc80a8cb208bb1e1c00f779
Anjali Mittal <mittalanjali1997@@gmail.com>
2020-08-03 22:00:52 +0530

    My Protractor Project

---
+++ tests/calculator.js
@@ -0,0 +1,51 @@
+let homepage = require('../pages/homepage');
+
+describe('demo calculator tests', function () {
+
+    it('addition test', function () {
+
+        homepage.get('http://juliemr.github.io/protractor-demo/');
+
+        homepage.enterFirstNumber('4');
+
+        homepage.enterSecondNumber('3');
+
+        homepage.clickGo();
+
+        homepage.verifyResult('7');
+
+        browser.sleep(2000)
+    });
+
+    it('subtraction test', function () {
+
+        homepage.get('http://juliemr.github.io/protractor-demo/');
+
+        homepage.enterFirstNumber('4');
+
+        homepage.enterSecondNumber('3');
+
+        homepage.clickGo1();
+
+        homepage.verifyResult('7');
+
+        browser.sleep(2000)
+
+    });
+
+});
+
+/*
+describe('demo calculator tests', function (){
+    it('addition test', function () {
+        browser.get('http://juliemr.github.io/protractor-demo/');
+        element(by.model('first')).sendKeys('2');
+        element(by.model('second')).sendKeys('3');
+        element(by.css('[ng-click="doAddition()"]')).click();
+        let result =element(by.cssContainingText('.ng-binding','5'));
+        expect(result.getText()).toEqual('5');
+        browser.sleep(2000)
+
+    }); 
+});
+*/
